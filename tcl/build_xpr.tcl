1c13836c24aa2f8d8a2c94a34bb58278902d606e
Michael Mulhearn <michaeljmulhearn@gmail.com>
2019-12-27 16:20:07 -0800

    generate top-level VHDL wrapper on-the-fly

--- tcl/build_xpr.tcl
+++ tcl/build_xpr.tcl
@@ -5,7 +5,23 @@
 #
 #     products/zsys_wrapper.hdf
 #
+
+# open the project:
 open_project ./pacman-fw/pacman-fw.xpr
+
+# delete the top-level wrapper if it already exists:
+set file "[get_files -quiet "zsys_wrapper.vhd"]"
+if { $file != "" } { 
+    puts "deleting top-level wrapper"
+    remove_files  $file
+    file delete -force $file
+}
+
+# recrete the top-level wrapper
+puts "recreating top-level wrapper"
+make_wrapper -files [get_files zsys.bd] -top
+add_files -norecurse pacman-fw/pacman-fw.srcs/sources_1/bd/zsys/hdl/zsys_wrapper.vhd
+
 update_compile_order -fileset sources_1
 reset_run synth_1
 reset_run impl_1
