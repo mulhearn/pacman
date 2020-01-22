869714da1c30ec48ca17c61de08d504822c67ea3
Michael Mulhearn <michaeljmulhearn@gmail.com>
2020-01-22 00:49:56 -0800

    workarounds for trenz fsbl no longer working

--- petalinux/build.sh
+++ petalinux/build.sh
@@ -2,7 +2,9 @@
 cd petalinux
 petalinux-config --get-hw-description=../products/ --oldconfig
 petalinux-build
-petalinux-package --boot --force --fsbl ../products/zynq_fsbl.elf --fpga images/linux/system.bit --u-boot
+# the TE customized version is no longer working for me... reverting to petalinux version for now
+#petalinux-package --boot --force --fsbl ../products/zynq_fsbl.elf --fpga images/linux/system.bit --u-boot
+petalinux-package --boot --force --fsbl build/tmp/sysroots-components/plnx_zynq7/fsbl/boot/fsbl.elf --fpga images/linux/system.bit --u-boot
 # if you want to append local files to rootfs, just add them to local/root:
 source local/update.sh
 cd ..
