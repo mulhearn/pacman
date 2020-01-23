0c5061fb97a693adb4abdd5053d4d2d943b2b366
Michael Mulhearn <michaeljmulhearn@gmail.com>
2020-01-22 17:08:44 -0800

    fix hello world not programming FPGA

--- srcs/sw/bareapp/tcl/run.tcl
+++ srcs/sw/bareapp/tcl/run.tcl
@@ -1,13 +1,15 @@
 # Set SDK workspace
 setws .
 # Build all projects
-projects -build
+#projects -build
 # Connect to a remote hw_server
 connect
 # Select a target
 targets -set -nocase -filter {name =~ "ARM*#0"}
 # System Reset
 rst -system
+# Configure the FPGA
+fpga ./hw1/zsys_wrapper.bit 
 # PS7 initialization
 namespace eval xsdb {source hw1/ps7_init.tcl; ps7_init}
 # Download the elf
