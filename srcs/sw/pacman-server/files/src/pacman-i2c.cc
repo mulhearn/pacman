f18f0b0627c4500241dec0bdc492d6c46e0c2dd5
peter-madigan <pmadigan@berkeley.edu>
2020-05-22 10:03:36 -0700

    Fix endianness in i2c read, and remove i2c debugging print statements

--- srcs/sw/pacman-server/files/src/pacman-i2c.cc
+++ srcs/sw/pacman-server/files/src/pacman-i2c.cc
@@ -31,6 +31,7 @@ int i2c_send(int fh, uint8_t addr, uint8_t reg) {
     if (i2c_addr(fh, addr) < 0) return -1;
     uint8_t buf[1];
     buf[0] = reg;
+    //printf("i2c_send %d\n", buf[0]);    
     return write(fh,buf,1);
 }
 
@@ -38,8 +39,9 @@ int i2c_set(int fh, uint32_t addr, uint8_t reg, uint16_t val) {
     if (i2c_addr(fh, addr) < 0) return -1;
     uint8_t buf[3];
     buf[0] = reg;
-    buf[1] = val & 0xFF00 >> 8;
+    buf[1] = (val & 0xFF00) >> 8;
     buf[2] = val & 0x00FF;
+    //printf("i2c_set %d %d %d\n", buf[0], buf[1], buf[2]);
     return write(fh,buf,3);
 }
 
@@ -54,9 +56,13 @@ int i2c_recv(int fh, uint32_t addr, uint8_t reg, uint8_t* buf, uint32_t nbytes) 
         printf("Failed to read register!\n");
         return -1;
     }
+    //printf("i2c_recv addr x%02x reg x%02x read: ",addr,reg);
+    //for (int i = 0; i < nbytes; i++) printf("x%02x ",buf[i]);
+    //printf("\n");
     return nbytes;
 }
 
+#define READ_BYTES 4
 uint32_t i2c_read(int fh, uint32_t reg) {
     if (reg < I2C_BASE_ADDR || reg >= I2C_BASE_ADDR+I2C_BASE_LEN) {
         printf("Bad i2c address: 0x%08x\n", reg);
@@ -65,24 +71,32 @@ uint32_t i2c_read(int fh, uint32_t reg) {
     uint32_t offset = (reg - I2C_BASE_ADDR) & 0xFFFFFFF0;
     uint8_t i2c_addr = 0;
     uint8_t i2c_reg = (reg - I2C_BASE_ADDR) & 0x0000000F;
-    uint8_t buf[2];
+    uint8_t buf[READ_BYTES];
     if (offset == OFFSET_DAC_VDDD) {
+        //printf("Read from VDDD DAC\n");
         i2c_addr = ADDR_DAC_VDDD;
     } else if (offset == OFFSET_DAC_VDDA) {
+        //printf("Read from VDDA DAC\n");
         i2c_addr = ADDR_DAC_VDDA;
     } else if (offset == OFFSET_ADC_VPLUS) {
+        //printf("Read from V+ ADC\n");
         i2c_addr = ADDR_ADC_VPLUS;
     } else if (offset == OFFSET_ADC_VDDD) {
+        //printf("Read from VDDD ADC\n");
         i2c_addr = ADDR_ADC_VDDD;
     } else if (offset == OFFSET_ADC_VDDA) {
+        //printf("Read from VDDA ADC\n");
         i2c_addr = ADDR_ADC_VDDA;
     } else {
         printf("Access empty i2c register: 0x%08x\n", reg);
         return 0;
     }
-    uint32_t rv;
-    i2c_recv(fh, i2c_addr, i2c_reg, buf, 2);    
-    memcpy(buf, &rv, 2);
+    uint32_t rv = 0;
+    i2c_recv(fh, i2c_addr, i2c_reg, buf, READ_BYTES);
+    for (int i = 0; i < READ_BYTES; i++) {
+        rv = (rv << 8) + buf[i];
+    }
+    //printf("Read %d\n", rv);
     return rv;
 }
 
