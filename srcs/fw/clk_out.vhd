91ac2713152009b7d1714c9e6108b0c664a83ef9
peter-madigan <pmadigan@berkeley.edu>
2020-06-03 14:01:08 -0700

    Drive differential clk from zynq

--- srcs/fw/clk_out.vhd
+++ srcs/fw/clk_out.vhd
@@ -5,8 +5,9 @@ use ieee.numeric_std.all;
 entity clk_out is
 port
  (
-  i  : in    std_logic;
-  o : out   std_logic
+  i  : in    std_logic;  
+  o_p : out  std_logic;
+  o_n : out  std_logic
 );
 end clk_out;
 
@@ -27,6 +28,16 @@ architecture arch_imp of clk_out is
       S : in std_logic
       );
   end component;
+
+  component OBUFDS is
+    port (
+      O : out std_logic;
+      OB : out std_logic;
+      I : in std_logic
+      );
+  end component;
+
+  signal o : std_logic;
   
 begin
 
@@ -43,6 +54,12 @@ begin
       R              => '0',
       S              => '0'
       );
+
+  obufds_inst : OBUFDS port map (
+    O => o_p,
+    OB => o_n,
+    I => o
+    );
   
 end arch_imp;
 
