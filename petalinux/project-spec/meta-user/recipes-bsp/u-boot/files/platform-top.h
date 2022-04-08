4ae10d388d8cbef178c0b22fce0436c26146ab39
Michael Mulhearn <michaeljmulhearn@gmail.com>
2022-04-08 16:04:51 -0700

    first changes for rev4:  new constraints, remove differential signals no longer in HW, remove second i2c from PL,  boot from eMMC insead of SD card.  Successfully boots.

--- petalinux/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ petalinux/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -36,3 +36,5 @@
 #endif
 
 #define CONFIG_PREBOOT    "echo U-BOOT for petalinux;echo importing env from FSBL shared area at 0xFFFFFC00; if itest *0xFFFFFC00 == 0xCAFEBABE; then echo Found valid magic; env import -t 0xFFFFFC04; fi;setenv preboot; echo; dhcp"
+
+#define CONFIG_BOOTCOMMAND  "ext4load mmc 1 0x08000000 image.ub ; bootm 0x08000000"
