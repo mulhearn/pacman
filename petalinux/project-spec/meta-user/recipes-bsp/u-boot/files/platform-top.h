322ed837d09a5516afa5aa4825519d5e50de6398
karcher <akarcher@lbl.gov>
2023-03-16 13:46:15 -0700

    added uboot and kernel custom config

--- petalinux/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ petalinux/project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -1,4 +1,3 @@
 #include <configs/zynq-common.h>
 #include <configs/platform-auto.h>
 
-#define CONFIG_PREBOOT    "echo U-BOOT for petalinux;echo importing env from FSBL shared area at 0xFFFFFC00; if itest *0xFFFFFC00 == 0xCAFEBABE; then echo Found valid magic; env import -t 0xFFFFFC04; fi;setenv preboot; echo; dhcp"
