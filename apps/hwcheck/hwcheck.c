2ee972bc5a678c8bf2674f9afe601e78b60948fc
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2024-10-30 22:27:25 -0700

    add blink LEDs test

--- apps/hwcheck/hwcheck.c
+++ apps/hwcheck/hwcheck.c
@@ -9,6 +9,7 @@
 #include "sleep.h"
 
 // MIO pinout:
+#define LEDA 7
 #define LED0 12
 #define LED1 13
 
@@ -32,6 +33,9 @@ int init_gpiops(){
     xil_printf("FAILED.\r\n");
     return XST_FAILURE;
   }
+  XGpioPs_SetDirectionPin(&gpiops, LEDA, 1);
+  XGpioPs_SetOutputEnablePin(&gpiops, LEDA, 1);
+  XGpioPs_WritePin(&gpiops, LEDA, 0x0);
   XGpioPs_SetDirectionPin(&gpiops, LED0, 1);
   XGpioPs_SetOutputEnablePin(&gpiops, LED0, 1);
   XGpioPs_WritePin(&gpiops, LED0, 0x0);
@@ -47,10 +51,11 @@ int init_gpiops(){
 // Device initialization:
 // I2C:
 #define IIC_DEVICE_ID     XPAR_XIICPS_0_DEVICE_ID
+//#define IIC_DEVICE_ID     XPAR_XIICPS_1_DEVICE_ID
 #define IIC_SCLK_RATE     200000
 XIicPs iicps;
 
-int init_iic(){  
+int init_iic(){
   xil_printf("initializing I2C interface...");
   XIicPs_Config *cfg = XIicPs_LookupConfig(IIC_DEVICE_ID);
   if (NULL == cfg) {
@@ -63,7 +68,7 @@ int init_iic(){
     return XST_FAILURE;
   }
   xil_printf("success.\r\n");
-  xil_printf("perfomring I2C selftest...");
+  xil_printf("performing I2C selftest...");
   status = XIicPs_SelfTest(&iicps);
   if (status != XST_SUCCESS)  {
     xil_printf("FAILED.\r\n");
@@ -85,12 +90,12 @@ void iic_send(unsigned addr, unsigned reg){
 
   xil_printf("I2C:  sending register 0x%x at address 0x%x...", reg, addr);
   int status = XIicPs_MasterSendPolled
-    (&iicps, &buf, 1, addr);  
+    (&iicps, &buf, 1, addr);
   if (status != XST_SUCCESS) {
     xil_printf("failed.\r\n");
   } else {
     xil_printf("success.\r\n");
-  } 
+  }
   xil_printf("I2C:  waiting for bus...");
   while (XIicPs_BusIsBusy(&iicps)) {
     /* NOP */
@@ -106,17 +111,17 @@ void iic_set(unsigned addr, unsigned reg, unsigned up, unsigned dn){
 
   xil_printf("I2C:  setting register 0x%x to 0x %x %x at address 0x%x...", reg, up, dn, addr);
   int status = XIicPs_MasterSendPolled
-    (&iicps, buf, 3, addr);  
+    (&iicps, buf, 3, addr);
   if (status != XST_SUCCESS) {
     xil_printf("failed.\r\n");
   } else {
     xil_printf("success.\r\n");
-  } 
+  }
   xil_printf("I2C:  waiting for bus...");
   while (XIicPs_BusIsBusy(&iicps)) {
     /* NOP */
   }
-  xil_printf("done.\r\n");  
+  xil_printf("done.\r\n");
 }
 
 void iic_byte(unsigned addr, unsigned reg, unsigned byte){
@@ -126,17 +131,17 @@ void iic_byte(unsigned addr, unsigned reg, unsigned byte){
 
   xil_printf("I2C:  setting register 0x%x to 0x %x at address 0x%x...", reg, byte, addr);
   int status = XIicPs_MasterSendPolled
-    (&iicps, buf, 2, addr);  
+    (&iicps, buf, 2, addr);
   if (status != XST_SUCCESS) {
     xil_printf("failed.\r\n");
   } else {
     xil_printf("success.\r\n");
-  } 
+  }
   xil_printf("I2C:  waiting for bus...");
   while (XIicPs_BusIsBusy(&iicps)) {
     /* NOP */
   }
-  xil_printf("done.\r\n");  
+  xil_printf("done.\r\n");
 }
 
 
@@ -147,9 +152,9 @@ unsigned iic_recv(unsigned addr, unsigned reg, unsigned nbytes){
   for (int i=0; i<nbytes; i++){
     buff[i] = 0;
   }
-  xil_printf("I2C:  reading back register...");    
+  xil_printf("I2C:  reading back register...");
   status = XIicPs_MasterRecvPolled
-    (&iicps, buff, nbytes, addr);  
+    (&iicps, buff, nbytes, addr);
   if (status != XST_SUCCESS) {
     xil_printf("failed.\r\n");
   } else {
@@ -159,23 +164,30 @@ unsigned iic_recv(unsigned addr, unsigned reg, unsigned nbytes){
   unsigned value = 0;
   for (int i=0; i<nbytes; i++){
     xil_printf("0x%x  ", buff[i]);
-    value = (value<<8) + buff[i];    
-  }  
-  xil_printf("--> 0x%x (%d)\r\n", value, value);  
+    value = (value<<8) + buff[i];
+  }
+  xil_printf("--> 0x%x (%d)\r\n", value, value);
   return value;
 }
 
 void blink(){
   static const int nblink = 5;
   static const int wait_usec = 100000;
-  
+
+  xil_printf("BLINK LEDS:  blinking LED 1 (MIO pin)...\r\n");
+  for (int iblink=0; iblink<nblink; iblink++){
+    XGpioPs_WritePin(&gpiops, LEDA, 1);
+    usleep(wait_usec);
+    XGpioPs_WritePin(&gpiops, LEDA, 0);
+    usleep(wait_usec);
+  }
   xil_printf("BLINK LEDS:  blinking LED 1 (MIO pin)...\r\n");
   for (int iblink=0; iblink<nblink; iblink++){
     XGpioPs_WritePin(&gpiops, LED0, 1);
     usleep(wait_usec);
     XGpioPs_WritePin(&gpiops, LED0, 0);
     usleep(wait_usec);
-  }  
+  }
   xil_printf("BLINK LEDS:  blinking LED 2 (MIO pin)...\r\n");
   for (int iblink=0; iblink<nblink; iblink++){
     XGpioPs_WritePin(&gpiops, LED1, 1);
@@ -183,7 +195,7 @@ void blink(){
     XGpioPs_WritePin(&gpiops, LED1, 0);
     usleep(wait_usec);
   }
-  xil_printf("BLINK LEDS:  done.\r\n");  
+  xil_printf("BLINK LEDS:  done.\r\n");
 }
 
 void check_iic(){
@@ -217,21 +229,21 @@ void check_reg_ro(){
 
 void check_reg_rw(){
   static unsigned count=0;
-  
+
   xil_printf("Count is 0x%x  \r\n", count);
 
   if ((count % 2)){
     Xil_Out32(ADDR_AXIL_REGS+0xFF00, 0x0);
     Xil_Out32(ADDR_AXIL_REGS+0x0000, 0x0);
-    Xil_Out32(ADDR_AXIL_REGS+0x1000, 0x0);    
+    Xil_Out32(ADDR_AXIL_REGS+0x1000, 0x0);
   } else {
     Xil_Out32(ADDR_AXIL_REGS+0xFF00, 0xAAAA1111);
     Xil_Out32(ADDR_AXIL_REGS+0x0000, 0xBBBB2222);
-    Xil_Out32(ADDR_AXIL_REGS+0x1000, 0xCCCC4444);        
+    Xil_Out32(ADDR_AXIL_REGS+0x1000, 0xCCCC4444);
   }
   Xil_Out32(ADDR_AXIL_REGS+0xFF04, 0xDDDD0000 + count);
   Xil_Out32(ADDR_AXIL_REGS+0x0004, 0xEEEE0000 + count*2);
-  Xil_Out32(ADDR_AXIL_REGS+0x1004, 0xFFFF0000 + count);        
+  Xil_Out32(ADDR_AXIL_REGS+0x1004, 0xFFFF0000 + count);
 
   count = (count + 1)&0xF;
 }
@@ -284,10 +296,10 @@ void read_rx(){
   xil_printf(" *** READING RX REGISTERS *** \r\n");
 
   unsigned chan=0;
-  //for (unsigned chan=0; chan<40; chan++){    
+  //for (unsigned chan=0; chan<40; chan++){
   unsigned chan_off = (chan<<10) + (0x1<<9);
   unsigned addr = 0;
-    
+
   xil_printf("UART RX channel:  %d   Address Offset:  0x%x \r\n", chan, chan_off);
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_STATUS;
   xil_printf("status (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));
@@ -296,7 +308,7 @@ void read_rx(){
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_LOOK_A;
   xil_printf("reg A (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_LOOK_B;
-  xil_printf("reg B (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));    
+  xil_printf("reg B (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_LOOK_C;
   xil_printf("reg C (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_LOOK_D;
@@ -321,7 +333,7 @@ void read_rx(){
 void single_rx(){
 
   unsigned chan=0;
-  //for (unsigned chan=0; chan<40; chan++){    
+  //for (unsigned chan=0; chan<40; chan++){
   unsigned chan_off = (chan<<10) + (0x1<<9);
   unsigned addr = 0;
   unsigned val = 0;
@@ -330,7 +342,7 @@ void single_rx(){
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_COMMAND;
   val  = 0x1;
   xil_printf("sending command(0x%x) with value 0x%x  \r\n", addr, val);
-  Xil_Out32(addr, val);      
+  Xil_Out32(addr, val);
   //}
 }
 
@@ -340,7 +352,7 @@ void read_tx(){
   xil_printf(" *** READING TX REGISTERS *** \r\n");
 
   unsigned chan=0;
-  //for (unsigned chan=0; chan<40; chan++){    
+  //for (unsigned chan=0; chan<40; chan++){
   unsigned chan_off = (chan<<10);
   unsigned addr = 0;
   unsigned status, config;
@@ -369,15 +381,15 @@ void read_tx(){
   xil_printf("status(2)  -         ack - %d\r\n", (status&0x004)!=0);
   xil_printf("status(3)  -          tx - %d\r\n", (status&0x008)!=0);
   xil_printf("\r\n");
-  xil_printf("status(4)  -  valid_seen - %d\r\n", (status&0x010)!=0); 
-  xil_printf("status(5)  -   busy_seen - %d\r\n", (status&0x020)!=0); 
-  xil_printf("status(6)  -    ack_seen - %d\r\n", (status&0x040)!=0);  
-  xil_printf("status(7)  -     tx_seen - %d\r\n", (status&0x080)!=0);   
+  xil_printf("status(4)  -  valid_seen - %d\r\n", (status&0x010)!=0);
+  xil_printf("status(5)  -   busy_seen - %d\r\n", (status&0x020)!=0);
+  xil_printf("status(6)  -    ack_seen - %d\r\n", (status&0x040)!=0);
+  xil_printf("status(7)  -     tx_seen - %d\r\n", (status&0x080)!=0);
   xil_printf("\r\n");
   xil_printf("status(8)  - single_seen - %d\r\n", (status&0x100)!=0);
-  xil_printf("status(9)  -  start_seen - %d\r\n", (status&0x200)!=0); 
-  xil_printf("status(10) -  stop_seen  - %d\r\n", (status&0x400)!=0);  
-  xil_printf("status(11) -    running  - %d\r\n", (status&0x800)!=0);  
+  xil_printf("status(9)  -  start_seen - %d\r\n", (status&0x200)!=0);
+  xil_printf("status(10) -  stop_seen  - %d\r\n", (status&0x400)!=0);
+  xil_printf("status(11) -    running  - %d\r\n", (status&0x800)!=0);
   xil_printf("\r\n");
   unsigned count = (0xFFFF0000&status)>>16;
   xil_printf("status(31 ... 16) -- count:  0x%x (%d)\r\n", count, count);
@@ -390,7 +402,7 @@ void read_tx(){
 
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_CNT_ACK;
   xil_printf("ack count   (0x%x) -- 0x%x  \r\n", addr, Xil_In32(addr));
-  
+
   //}
 }
 
@@ -399,12 +411,12 @@ void single_tx(){
   count = count + 1;
 
   unsigned chan=0;
-  //for (unsigned chan=0; chan<40; chan++){    
+  //for (unsigned chan=0; chan<40; chan++){
   unsigned chan_off = (chan<<10);
   unsigned val = 0;
   unsigned addr = 0;
 
-  
+
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_CONFIG;
   val  = 0x00002001;
   xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
@@ -413,22 +425,22 @@ void single_tx(){
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_SEND_C;
   val  = 0xCCCC0000 + count;
   xil_printf("setting SEND C (0x%x) to 0x%x  \r\n", addr, val);
-  Xil_Out32(addr, val);  
+  Xil_Out32(addr, val);
 
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_SEND_D;
   val  = 0xDDDD0000 + count;
   xil_printf("setting SEND D  (0x%x) to 0x%x  \r\n", addr, val);
-  Xil_Out32(addr, val);  
+  Xil_Out32(addr, val);
 
   addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_COMMAND;
   val  = 0x1;
   xil_printf("sending command(0x%x) with value 0x%x  \r\n", addr, val);
-  Xil_Out32(addr, val);  
+  Xil_Out32(addr, val);
   //}
 }
 
 void send_command(unsigned mask){
-  for (unsigned chan=0; chan<40; chan++){    
+  for (unsigned chan=0; chan<40; chan++){
     unsigned chan_off = (chan<<10);
     unsigned val = 0;
     unsigned addr = 0;
@@ -436,7 +448,7 @@ void send_command(unsigned mask){
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_COMMAND;
     val  = mask;
     //xil_printf("sending command(0x%x) with value 0x%x  \r\n", addr, val);
-    Xil_Out32(addr, val);  
+    Xil_Out32(addr, val);
   }
 
   for (unsigned chan=0; chan<40; chan++){
@@ -447,7 +459,7 @@ void send_command(unsigned mask){
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_COMMAND;
     val  = mask;
     //xil_printf("sending command(0x%x) with value 0x%x  \r\n", addr, val);
-    Xil_Out32(addr, val);  
+    Xil_Out32(addr, val);
   }
 }
 
@@ -455,11 +467,11 @@ void send_command(unsigned mask){
 void set_mode_single(){
 
   // config TX
-  for (unsigned chan=0; chan<40; chan++){    
+  for (unsigned chan=0; chan<40; chan++){
     unsigned chan_off = (chan<<10);
     unsigned val = 0;
     unsigned addr = 0;
-  
+
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_CONFIG;
     val  = 0x00002001;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
@@ -467,22 +479,22 @@ void set_mode_single(){
   }
 
   // config RX
-  for (unsigned chan=0; chan<40; chan++){    
+  for (unsigned chan=0; chan<40; chan++){
     unsigned chan_off = (chan<<10) + (0x1<<9);
     unsigned val = 0;
     unsigned addr = 0;
-  
+
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_CONFIG;
     val  = 0x00002001;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
     Xil_Out32(addr, val);
   }
-  
+
 }
 
 void set_mode_continuous(){
-  
-  for (unsigned chan=0; chan<40; chan++){    
+
+  for (unsigned chan=0; chan<40; chan++){
     unsigned chan_off = (chan<<10);
     unsigned val = 0;
     unsigned addr = 0;
@@ -490,13 +502,13 @@ void set_mode_continuous(){
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_SEND_C;
     val  = 0xCCCC1111;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
-    Xil_Out32(addr, val);  
+    Xil_Out32(addr, val);
 
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_TX_SEND_D;
     val  = 0xDDDD2222;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
-    Xil_Out32(addr, val);  
-  
+    Xil_Out32(addr, val);
+
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_CONFIG;
     val  = 0x00003001;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
@@ -504,16 +516,16 @@ void set_mode_continuous(){
   }
 
   // config RX
-  for (unsigned chan=0; chan<40; chan++){    
+  for (unsigned chan=0; chan<40; chan++){
     unsigned chan_off = (chan<<10) + (0x1<<9);
     unsigned val = 0;
     unsigned addr = 0;
-  
+
     addr = ADDR_AXIL_REGS+chan_off+C_ADDR_RX_CONFIG;
     val  = 0x00003001;
     xil_printf("setting config (0x%x) to 0x%x  \r\n", addr, val);
     Xil_Out32(addr, val);
-  }  
+  }
 }
 
 
@@ -533,7 +545,7 @@ void dma_loopback(){
   u32 *rx_buf = (u32 *)rx_base;
   unsigned timeout;
   unsigned words = 4;
-  
+
   // Using XPAR_AXI_DMA_0_BASEADDR  defined in xparameters.h
 
   xil_printf("*** Sending DMA reset*** \r\n");
@@ -563,8 +575,8 @@ void dma_loopback(){
   Xil_Out32(XPAR_AXI_DMA_0_BASEADDR+0x30, 0x01);
 
   dma_status();
-  
-  xil_printf("*** Preparing buffers*** \r\n");  
+
+  xil_printf("*** Preparing buffers*** \r\n");
   tx_buf[0] = 0xBBBBAAAA;
   tx_buf[1] = 0xDDDDCCCC;
   tx_buf[2] = 0x11111111;
@@ -573,18 +585,18 @@ void dma_loopback(){
   for (int i=0; i<words; i++)
     rx_buf[i] = 0x0;
 
-  
+
   Xil_DCacheFlushRange((UINTPTR)tx_buf, words*4);
   Xil_DCacheFlushRange((UINTPTR)rx_buf, words*4);
-  
-  xil_printf("*** Sending write *** \r\n");    
+
+  xil_printf("*** Sending write *** \r\n");
   Xil_Out32(XPAR_AXI_DMA_0_BASEADDR+0x18, (u32) tx_buf);
   Xil_Out32(XPAR_AXI_DMA_0_BASEADDR+0x28, words*4);
 
   timeout = 10;
   while(timeout){
     unsigned sr_mm2s = Xil_In32(XPAR_AXI_DMA_0_BASEADDR+0x04);
-    if ((sr_mm2s&0x2)!=0) 
+    if ((sr_mm2s&0x2)!=0)
       break;
     xil_printf("*** waiting for idle *** \r\n");
     timeout--;
@@ -593,15 +605,15 @@ void dma_loopback(){
     xil_printf("*** ERROR:  failed to reach idle before timeout! *** \r\n");
     return;
   }
-  
-  xil_printf("*** Sending read *** \r\n");    
+
+  xil_printf("*** Sending read *** \r\n");
   Xil_Out32(XPAR_AXI_DMA_0_BASEADDR+0x48, (u32) rx_buf);
   Xil_Out32(XPAR_AXI_DMA_0_BASEADDR+0x58, words*4);
 
   timeout = 10;
   while(timeout){
     unsigned sr_s2mm = Xil_In32(XPAR_AXI_DMA_0_BASEADDR+0x34);
-    if ((sr_s2mm&0x2)!=0) 
+    if ((sr_s2mm&0x2)!=0)
       break;
     xil_printf("*** waiting for idle *** \r\n");
     timeout--;
@@ -612,11 +624,11 @@ void dma_loopback(){
   }
 
   dma_status();
-  
+
   Xil_DCacheInvalidateRange((UINTPTR) rx_buf, words*4);
   for (int i=0; i<words; i++)
     xil_printf("sent[%d]:  0x%x --> received[%d]:  0x%x\r\n", i, tx_buf[i], i, rx_buf[i]);
-  
+
 }
 
 
@@ -628,7 +640,7 @@ void dma_loopback_xilinx(){
   u32 *tx_buf = (u32 *)tx_base;
   u32 *rx_buf = (u32 *)rx_base;
   unsigned words = 4;
-  
+
   XAxiDma AxiDma;
   XAxiDma_Config *CfgPtr;
 
@@ -641,7 +653,7 @@ void dma_loopback_xilinx(){
     xil_printf("No config found for %d\r\n", XPAR_AXIDMA_0_DEVICE_ID);
     return;
   }
-	
+
   Status = XAxiDma_CfgInitialize(&AxiDma, CfgPtr);
   if (Status != XST_SUCCESS) {
     xil_printf("Initialization failed %d\r\n", Status);
@@ -655,14 +667,14 @@ void dma_loopback_xilinx(){
 
   for (int i=0; i<words; i++)
     rx_buf[i] = 0x0;
-    
+
   Xil_DCacheFlushRange((UINTPTR)tx_buf, words*4);
   Xil_DCacheFlushRange((UINTPTR)rx_buf, words*4);
 
   xil_printf("simple transfer: TX\r\n");
   Status = XAxiDma_SimpleTransfer(&AxiDma, (UINTPTR) rx_buf,
 				  words*4, XAXIDMA_DEVICE_TO_DMA);
-  
+
   if (Status != XST_SUCCESS) {
     xil_printf("dma_loopback failure\r\n");
   }
@@ -674,7 +686,7 @@ void dma_loopback_xilinx(){
   if (Status != XST_SUCCESS) {
     xil_printf("dma_loopback failure\r\n");
   }
-  
+
   /*Wait till tranfer is done or 1usec * 10^6 iterations of timeout occurs*/
   int TimeOut = 1000;
   while (TimeOut) {
@@ -688,18 +700,18 @@ void dma_loopback_xilinx(){
   }
 
   Xil_DCacheInvalidateRange((UINTPTR) rx_buf, words*4);
-  
+
   for (int i=0; i<words; i++)
     xil_printf("sent[%d]:  0x%x --> received[%d]:  0x%x\r\n", i, tx_buf[i], i, rx_buf[i]);
 
 }
-		
+
 int main(){
   xil_printf("SANITY NUMBER:  1\r\n");
   xil_printf("Pac-Man Card Low-Level Hardware Testing (Development)\r\n");
   int status = 0;
   status |= init_gpiops();
-  status |= init_iic();    
+  status |= init_iic();
   if (status != XST_SUCCESS) {
     xil_printf("Hardware initialization has FAILED.\r\n");
     return 0;
@@ -711,7 +723,8 @@ int main(){
     xil_printf("(5) read RX registers (6) single RX \r\n");
     xil_printf("(7) start (8) stop (9) clear\r\n");
     xil_printf("(a) DMA status (b) DMA loopback (c) DMA loopback (Xilinx) \r\n");
-      
+    xil_printf("(d) blink LEDs \r\n");
+
     unsigned char c=inbyte();
     xil_printf("pressed:  %c\n\r", c);
     switch(c){
@@ -744,20 +757,20 @@ int main(){
       break;
     case 'a':
       dma_status();
-      break;	
+      break;
     case 'b':
       dma_loopback();
-      break;	
+      break;
     case 'c':
       dma_loopback_xilinx();
-      break;	
+      break;
+    case 'd':
+      blink();
+      break;
+
     default:
       xil_printf("invalid selection...\n\r");
     }
   }
   return 0;
 }
-
-
-
-
