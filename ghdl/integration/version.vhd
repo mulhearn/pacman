18df2fd31af8a0e51fe588697471356ee26e0feb
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-06-19 18:18:42 -0700

    bump version

--- ghdl/integration/version.vhd
+++ ghdl/integration/version.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 package version is
   constant C_FW_MAJOR : integer  := 3;
-  constant C_FW_MINOR : integer  := 2;
+  constant C_FW_MINOR : integer  := 3;
   constant C_FW_BUILD : integer  := 16#7FFF0000#;
   constant C_HW_CODE  : integer  := 16#00010500#;
 end package version;
