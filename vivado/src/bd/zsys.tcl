305cdc5d7eb36980455aef9ff02f5e8773c50cb4
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-06-25 10:56:38 -0700

    working commit - switch DMA to scatter gather in HW only, breaking the simple-mode DMA drivers

--- vivado/src/bd/zsys.tcl
+++ vivado/src/bd/zsys.tcl
@@ -579,9 +579,10 @@ proc create_root_design { parentCell } {
   set axi_dma_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 axi_dma_0 ]
   set_property -dict [list \
     CONFIG.c_include_s2mm {1} \
-    CONFIG.c_include_sg {0} \
+    CONFIG.c_include_sg {1} \
     CONFIG.c_m_axis_mm2s_tdata_width {128} \
     CONFIG.c_mm2s_burst_size {256} \
+    CONFIG.c_s2mm_burst_size {256} \
     CONFIG.c_s_axis_s2mm_tdata_width {128} \
     CONFIG.c_sg_length_width {20} \
   ] $axi_dma_0
@@ -611,7 +612,10 @@ proc create_root_design { parentCell } {
   
   # Create instance: ps7_0_axi_periph, and set properties
   set ps7_0_axi_periph [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 ps7_0_axi_periph ]
-  set_property CONFIG.NUM_MI {3} $ps7_0_axi_periph
+  set_property -dict [list \
+    CONFIG.NUM_MI {3} \
+    CONFIG.NUM_SI {2} \
+  ] $ps7_0_axi_periph
 
 
   # Create instance: rst_ps7_0_100M, and set properties
@@ -630,10 +634,9 @@ proc create_root_design { parentCell } {
   set_property -dict [list \
     CONFIG.FIFO_DEPTH {16384} \
     CONFIG.FIFO_MEMORY_TYPE {auto} \
-    CONFIG.FIFO_MODE {1} \
+    CONFIG.FIFO_MODE {2} \
     CONFIG.HAS_RD_DATA_COUNT {1} \
     CONFIG.HAS_TKEEP {0} \
-    CONFIG.HAS_TLAST {1} \
     CONFIG.HAS_TSTRB {0} \
     CONFIG.HAS_WR_DATA_COUNT {1} \
     CONFIG.TDATA_NUM_BYTES {16} \
@@ -702,6 +705,7 @@ proc create_root_design { parentCell } {
   connect_bd_intf_net -intf_net axi_dma_0_M_AXIS_MM2S [get_bd_intf_pins tx_unit_0/S_AXIS] [get_bd_intf_pins axi_dma_0/M_AXIS_MM2S]
   connect_bd_intf_net -intf_net axi_dma_0_M_AXI_MM2S [get_bd_intf_pins axi_dma_0/M_AXI_MM2S] [get_bd_intf_pins axi_mem_intercon/S00_AXI]
   connect_bd_intf_net -intf_net axi_dma_0_M_AXI_S2MM [get_bd_intf_pins axi_dma_0/M_AXI_S2MM] [get_bd_intf_pins axi_mem_intercon/S01_AXI]
+  connect_bd_intf_net -intf_net axi_dma_0_M_AXI_SG [get_bd_intf_pins axi_dma_0/M_AXI_SG] [get_bd_intf_pins ps7_0_axi_periph/S01_AXI]
   connect_bd_intf_net -intf_net axi_mem_intercon_M00_AXI [get_bd_intf_pins axi_mem_intercon/M00_AXI] [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
   connect_bd_intf_net -intf_net axil_to_regbus_0_P_REGBUS [get_bd_intf_pins axil_to_regbus_0/P_REGBUS] [get_bd_intf_pins regbus_mux_0/S_REGBUS]
   connect_bd_intf_net -intf_net axis_data_fifo_0_M_AXIS [get_bd_intf_pins axis_data_fifo_0/M_AXIS] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
@@ -752,10 +756,10 @@ proc create_root_design { parentCell } {
   connect_bd_net -net global_unit_0_ANALOG_PWR_EN_O [get_bd_pins global_unit_0/ANALOG_PWR_EN_O] [get_bd_ports ANALOG_PWR_EN_O_0]
   connect_bd_net -net global_unit_0_LED_O [get_bd_pins global_unit_0/LED_O] [get_bd_ports LED_O_0]
   connect_bd_net -net global_unit_0_TILE_EN_O [get_bd_pins global_unit_0/TILE_EN_O] [get_bd_ports TILE_EN_O_0]
-  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins rst_ps7_0_100M/slowest_sync_clk] [get_bd_pins axi_dma_0/s_axi_lite_aclk] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins axil_to_regbus_0/S_AXI_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins axi_dma_0/m_axi_mm2s_aclk] [get_bd_pins axi_mem_intercon/S00_ACLK] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins axi_mem_intercon/M00_ACLK] [get_bd_pins axi_mem_intercon/ACLK] [get_bd_pins rx_unit_0/M_AXIS_ACLK] [get_bd_pins axi_dma_0/m_axi_s2mm_aclk] [get_bd_pins tx_unit_0/S_AXIS_ACLK] [get_bd_pins vio_0/clk] [get_bd_pins axi_mem_intercon/S01_ACLK] [get_bd_pins axis_data_fifo_0/s_axis_aclk] [get_bd_pins regbus_mux_0/ACLK] [get_bd_pins timing_unit_0/ACLK] [get_bd_pins global_unit_0/ACLK] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins adc_unit_0/ACLK]
+  connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins ps7_0_axi_periph/S00_ACLK] [get_bd_pins rst_ps7_0_100M/slowest_sync_clk] [get_bd_pins axi_dma_0/s_axi_lite_aclk] [get_bd_pins ps7_0_axi_periph/M00_ACLK] [get_bd_pins ps7_0_axi_periph/ACLK] [get_bd_pins axil_to_regbus_0/S_AXI_ACLK] [get_bd_pins ps7_0_axi_periph/M01_ACLK] [get_bd_pins axi_dma_0/m_axi_mm2s_aclk] [get_bd_pins axi_mem_intercon/S00_ACLK] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK] [get_bd_pins axi_mem_intercon/M00_ACLK] [get_bd_pins axi_mem_intercon/ACLK] [get_bd_pins rx_unit_0/M_AXIS_ACLK] [get_bd_pins axi_dma_0/m_axi_s2mm_aclk] [get_bd_pins tx_unit_0/S_AXIS_ACLK] [get_bd_pins vio_0/clk] [get_bd_pins axi_mem_intercon/S01_ACLK] [get_bd_pins axis_data_fifo_0/s_axis_aclk] [get_bd_pins regbus_mux_0/ACLK] [get_bd_pins timing_unit_0/ACLK] [get_bd_pins global_unit_0/ACLK] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk] [get_bd_pins ps7_0_axi_periph/M02_ACLK] [get_bd_pins adc_unit_0/ACLK] [get_bd_pins axi_dma_0/m_axi_sg_aclk] [get_bd_pins ps7_0_axi_periph/S01_ACLK]
   connect_bd_net -net processing_system7_0_FCLK_CLK1 [get_bd_pins processing_system7_0/FCLK_CLK1] [get_bd_pins tx_unit_0/UCLK_I] [get_bd_pins timing_unit_0/UCLK_I]
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_ps7_0_100M/ext_reset_in]
-  connect_bd_net -net rst_ps7_0_100M_peripheral_aresetn [get_bd_pins rst_ps7_0_100M/peripheral_aresetn] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins axil_to_regbus_0/S_AXI_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins axi_mem_intercon/S00_ARESETN] [get_bd_pins axi_mem_intercon/M00_ARESETN] [get_bd_pins axi_mem_intercon/ARESETN] [get_bd_pins rx_unit_0/M_AXIS_ARESETN] [get_bd_pins tx_unit_0/S_AXIS_ARESETN] [get_bd_pins axi_mem_intercon/S01_ARESETN] [get_bd_pins axis_data_fifo_0/s_axis_aresetn] [get_bd_pins regbus_mux_0/ARESETN] [get_bd_pins timing_unit_0/ARESETN] [get_bd_pins global_unit_0/ARESETN] [get_bd_pins axi_bram_ctrl_0/s_axi_aresetn] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins adc_unit_0/ARESETN]
+  connect_bd_net -net rst_ps7_0_100M_peripheral_aresetn [get_bd_pins rst_ps7_0_100M/peripheral_aresetn] [get_bd_pins ps7_0_axi_periph/S00_ARESETN] [get_bd_pins axi_dma_0/axi_resetn] [get_bd_pins ps7_0_axi_periph/M00_ARESETN] [get_bd_pins ps7_0_axi_periph/ARESETN] [get_bd_pins axil_to_regbus_0/S_AXI_ARESETN] [get_bd_pins ps7_0_axi_periph/M01_ARESETN] [get_bd_pins axi_mem_intercon/S00_ARESETN] [get_bd_pins axi_mem_intercon/M00_ARESETN] [get_bd_pins axi_mem_intercon/ARESETN] [get_bd_pins rx_unit_0/M_AXIS_ARESETN] [get_bd_pins tx_unit_0/S_AXIS_ARESETN] [get_bd_pins axi_mem_intercon/S01_ARESETN] [get_bd_pins axis_data_fifo_0/s_axis_aresetn] [get_bd_pins regbus_mux_0/ARESETN] [get_bd_pins timing_unit_0/ARESETN] [get_bd_pins global_unit_0/ARESETN] [get_bd_pins axi_bram_ctrl_0/s_axi_aresetn] [get_bd_pins ps7_0_axi_periph/M02_ARESETN] [get_bd_pins adc_unit_0/ARESETN] [get_bd_pins ps7_0_axi_periph/S01_ARESETN]
   connect_bd_net -net timing_unit_0_GLB_CLK_O [get_bd_pins timing_unit_0/GLB_CLK_O] [get_bd_ports GLB_CLK_O_0]
   connect_bd_net -net timing_unit_0_G_O [get_bd_pins timing_unit_0/G_O] [get_bd_ports SYNC_O_0]
   connect_bd_net -net timing_unit_0_TIMESTAMP_O [get_bd_pins timing_unit_0/TIMESTAMP_O] [get_bd_pins rx_unit_0/TIMESTAMP_I]
@@ -768,6 +772,11 @@ proc create_root_design { parentCell } {
   assign_bd_address -offset 0x40000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs axil_to_regbus_0/S_AXI/reg0] -force
   assign_bd_address -offset 0x00000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces axi_dma_0/Data_MM2S] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] -force
   assign_bd_address -offset 0x00000000 -range 0x40000000 -target_address_space [get_bd_addr_spaces axi_dma_0/Data_S2MM] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] -force
+  assign_bd_address -offset 0x42000000 -range 0x00002000 -target_address_space [get_bd_addr_spaces axi_dma_0/Data_SG] [get_bd_addr_segs axi_bram_ctrl_0/S_AXI/Mem0] -force
+
+  # Exclude Address Segments
+  exclude_bd_addr_seg -offset 0x40400000 -range 0x00010000 -target_address_space [get_bd_addr_spaces axi_dma_0/Data_SG] [get_bd_addr_segs axi_dma_0/S_AXI_LITE/Reg]
+  exclude_bd_addr_seg -offset 0x40000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces axi_dma_0/Data_SG] [get_bd_addr_segs axil_to_regbus_0/S_AXI/reg0]
 
 
   # Restore current instance
