44bd8b49c5ab803d94602f37c5ca65c97a0d7552
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-07-30 13:41:45 -0500

    add tx buffer timing diagram

--- ghdl/diagrams/timing_diagram.py
+++ ghdl/diagrams/timing_diagram.py
@@ -3,11 +3,12 @@ from vcdvcd import VCDVCD
 
 def get_time_series(vcd, name, tmin, tmax):
      tv = vcd[name].tv
-     ts = []
-     val = []
+     ts = []  # in nanoseconds
+     val = [] # as integer
      last_before_tmin = None
 
      for ts_fs, val_bin in tv:
+          # convert fs to ns:
           ts_ns = round(ts_fs / 1_000_000)
           if ts_ns < tmin:
                last_before_tmin = (ts_ns, val_bin)
@@ -20,7 +21,12 @@ def get_time_series(vcd, name, tmin, tmax):
      if last_before_tmin is not None:
           ts.insert(0, last_before_tmin[0])
           val.insert(0, int(last_before_tmin[1], 2))
-     
+
+     # If signal becomes constant before tmax, pad with a last value far past plot boundary
+     if ts and ts[-1] < tmax:
+          ts.append(tmax+100)
+          val.append(val[-1])
+
      return np.array(ts), np.array(val)
 
 def get_transitions(ts, slope_ns):
@@ -51,18 +57,16 @@ def get_time_series_single_bit(vcd, name, tmin, tmax):
      if (val[0]==0):
           return t, yup
      else:
-          return t, ydn               
+          return t, ydn
 
 def get_time_series_bus(vcd, name, tmin, tmax, slope_ns):
      ts, val     = get_time_series(vcd, name, tmin, tmax)
      t, yup, ydn = get_transitions(ts, slope_ns)
 
      if (ts[0] < tmin):
-          ts[0] = tmin          
+          ts[0] = tmin
      mask = (ts<tmax)
 
      ts = ts+slope_ns/2
-     
+
      return t, yup, ydn, ts[mask], val[mask]
-
-
