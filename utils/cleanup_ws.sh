7d8ecc36b5d8977ff04de9383e3774b4fc857a24
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-09-10 10:33:05 -0700

    updates to docs and utils

--- utils/cleanup_ws.sh
+++ utils/cleanup_ws.sh
@@ -5,6 +5,8 @@ find apps -type f -name '*.h' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 find apps vivado/tcl -type f -name '*.tcl' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 find docs -type f -name '*.txt' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 
+find petalinux/pkg -type f -name '*.c' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
+find petalinux/pkg -type f -name '*.h' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 find petalinux/pkg -type f -name '*.cc' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 find petalinux/pkg -type f -name '*.hh' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
 find petalinux/pkg -type f -name '*.bb' | xargs -n1 sed --in-place 's/[[:space:]]\+$//'
