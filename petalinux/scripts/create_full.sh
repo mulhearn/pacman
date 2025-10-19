144208913ba55ff2b98556e1a7fe9f4f2bf0a047
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-10-18 19:37:09 -0700

    add pacman-apps to rootfs

--- petalinux/scripts/create_full.sh
+++ petalinux/scripts/create_full.sh
@@ -37,7 +37,6 @@ cp -v $SRC/$SPEC/user-rootfsconfig $PROJ/project-spec/meta-user/conf/
 cp -v $SRC/$SPEC/rootfs_config     $PROJ/project-spec/configs/rootfs_config
 cp -v $SRC/$SPEC/config            $PROJ/project-spec/configs/config
 
-# add custom software packages
-cp -v -r $PKG/pacman-server      $PROJ/project-spec/meta-user/recipes-apps/
-cp -v -r $PKG/hwutil             $PROJ/project-spec/meta-user/recipes-apps/
+# add PACMAN software package:
+ln -sfn  ../../../../$PKG/pacman-apps $PROJ/project-spec/meta-user/recipes-apps/
 
