0a9ed3390b44b21cb2c92b58cdb916b994938b55
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-10-20 15:45:21 -0700

    add more reports to runsib

--- vivado/tcl/runsib.tcl
+++ vivado/tcl/runsib.tcl
@@ -23,6 +23,11 @@ open_run impl_1
 launch_runs impl_1 -to_step write_bitstream -jobs 2
 wait_on_run impl_1
 
-report_control_sets -hierarchical -hierarchical_depth 5 -file ../reports/control_sets.txt
+report_utilization -file ../reports/summary.txt
 report_utilization -hierarchical -hierarchical_depth 5 -file ../reports/utilization.txt
+report_control_sets -hierarchical -hierarchical_depth 5 -file ../reports/control_sets.txt
+report_timing_summary -file ../reports/timing_summary.txt
+report_timing -max_paths 10 -sort_by slack -delay_type max -nworst 10 -input_pins -significant_digits 3 -file ../reports/worst_timing_max.txt
+report_timing -max_paths 10 -sort_by slack -delay_type min -nworst 10 -input_pins -significant_digits 3 -file ../reports/worst_timing_min.txt
+
 write_hw_platform -fixed -include_bit -force -file ${origin_dir}/../products/pacman.xsa
