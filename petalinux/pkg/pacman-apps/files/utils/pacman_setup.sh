f45a65d5c37fdccbb9b77a9f24ff3947e24bac2b
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-10-19 18:04:03 -0700

    simple pacman setup script now finally bug free

--- petalinux/pkg/pacman-apps/files/utils/pacman_setup.sh
+++ petalinux/pkg/pacman-apps/files/utils/pacman_setup.sh
@@ -4,14 +4,13 @@
 #
 # initial setup of root account for PACMAN
 
-
 ROOT_HOME="/home/root"
 PYLIB_DIR="$ROOT_HOME/pylib"
-BASH_LINE='export PYTHONPATH=/home/root/pylib:$PYTHONPATH'
+PATH_LINE='export PYTHONPATH=/home/root/pylib:$PYTHONPATH'
 
 # ensure pylib exists
 mkdir -p "$PYLIB_DIR"
 
 # append pylib to .bashrc if not already present
-touch "$ROOT_HOME/.bashrc"
-grep -qxF "$BASH_LINE" "$ROOT_HOME/.bashrc" || echo "$BASH_LINE" >> "$ROOT_HOME/.bashrc"
+touch "$ROOT_HOME/.profile"
+grep -qxF "$PATH_LINE" "$ROOT_HOME/.profile" || echo "$PATH_LINE" >> "$ROOT_HOME/.profile"
