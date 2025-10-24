f958a7e05986b1501373991143f3f6b46607aaad
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-10-24 08:27:41 -0700

    revert to a simplified ADC unit

--- ghdl/adc/tb/adc_unit_tb.vhd
+++ ghdl/adc/tb/adc_unit_tb.vhd
@@ -13,7 +13,7 @@ architecture behaviour of adc_unit_tb is
   component adc_unit is
     port (
       ACLK	          : in std_logic;
-      ARESETN	          : in std_logic;
+      RST_I	          : in std_logic;
 
       -- REGBUS Ports
       S_REGBUS_RB_RUPDATE : in  std_logic;
@@ -27,12 +27,12 @@ architecture behaviour of adc_unit_tb is
       S_REGBUS_RB_WACK    : out std_logic;
 
       -- BRAM
-      BRAM_EN_O           : out std_logic;
-      BRAM_DATA_O         : out std_logic_vector(BRAM_DATA_WIDTH-1 downto 0);
-      BRAM_WEN_O          : out std_logic_vector(3 downto 0);
-      BRAM_ADDR_O         : out std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
-      BRAM_CLK_O          : out std_logic;
-      BRAM_RST_O          : out std_logic;
+      --BRAM_EN_O           : out std_logic;
+      --BRAM_DATA_O         : out std_logic_vector(BRAM_DATA_WIDTH-1 downto 0);
+      --BRAM_WEN_O          : out std_logic_vector(3 downto 0);
+      --BRAM_ADDR_O         : out std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
+      --BRAM_CLK_O          : out std_logic;
+      --BRAM_RST_O          : out std_logic;
 
       -- ADC
       ADC_EN_O            : out std_logic;
@@ -43,8 +43,8 @@ architecture behaviour of adc_unit_tb is
   end component;
 
   signal count     : integer := 0;
-  signal aclk      : std_logic;
-  signal aresetn   : std_logic;
+  signal clk       : std_logic;
+  signal rst       : std_logic;
 
   -- regbus
   -- read signals:
@@ -59,9 +59,9 @@ architecture behaviour of adc_unit_tb is
   signal wack    : std_logic := '0';
 
   -- daq
-  signal wen       : std_logic_vector(3 downto 0);
-  signal addr      : std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
-  signal do        : std_logic_vector(BRAM_DATA_WIDTH-1 downto 0);
+  --signal wen       : std_logic_vector(3 downto 0);
+  --signal addr      : std_logic_vector(BRAM_ADDR_WIDTH-1 downto 0);
+  --signal do        : std_logic_vector(BRAM_DATA_WIDTH-1 downto 0);
   signal di        : std_logic_vector(ADC_DATA_WIDTH-1 downto 0);
   signal diof      : std_logic;
 
@@ -71,10 +71,8 @@ architecture behaviour of adc_unit_tb is
 
 begin
   uut: adc_unit port map (
-    ADC_EN_O       => adc_en,
-    ADC_CLK_O      => adc_clk,
-    ACLK           => aclk,
-    ARESETN        => aresetn,
+    ACLK                => clk,
+    RST_I               => rst,
     S_REGBUS_RB_RUPDATE => rupdate,
     S_REGBUS_RB_RADDR   => raddr,
     S_REGBUS_RB_RDATA   => rdata,
@@ -85,107 +83,57 @@ begin
     S_REGBUS_RB_WACK    => wack,
     ADC_DATA_I          => di,
     ADC_DOF_I           => diof,
-    BRAM_DATA_O         => do,
-    BRAM_ADDR_O         => addr,
-    BRAM_WEN_O          => wen
+    ADC_EN_O            => adc_en,
+    ADC_CLK_O           => adc_clk
+    --BRAM_DATA_O         => do,
+    --BRAM_ADDR_O         => addr,
+    --BRAM_WEN_O          => wen
   );
 
-
-  aresetn_process : process
+  rst_process : process
   begin
-    aresetn <= '0';
-    wait for 12 ns;
-    aresetn <= '1';
+    rst <= '1';
+    wait for 20 ns;
+    rst <= '0';
     wait;
   end process;
 
-  aclk_process : process
+  clk_process : process
   begin
     count <= count + 1;
-    aclk <= '1';
+    clk <= '1';
     wait for 5 ns;
-    aclk <= '0';
+    clk <= '0';
     wait for 5 ns;
   end process;
 
-
-write_process : process
+  write_process : process
   begin
     wait for 1 ns;
-    wait for 20 ns; -- testing mode 0
-    waddr   <= x"D118";
-    wdata   <= x"00000005";
-    wupdate <= '1';
-    wait for 40 ns; -- testing mode 1
-    waddr   <= x"D110";
-    wdata   <= x"00080013";
-    wupdate <= '1';
-    wait for 50 ns;
-    waddr   <= x"D118";
-    wdata   <= x"00000005";
-    wupdate <= '1';
-    wait for 50 ns;
-    waddr   <= x"D114";
-    wdata   <= x"00000002";
-    wupdate <= '1';
-    wait for 40 ns; -- testing mode 2
-    waddr   <= x"D110";
-    wdata   <= x"00040023";
-    wupdate <= '1';
-    wait for 50 ns;
-    waddr   <= x"D118";
-    wdata   <= x"00000007";
-    wupdate <= '1';
-    wait for 50 ns;
-    waddr   <= x"D114";
-    wdata   <= x"00000004";
-    wupdate <= '1';
-    wait for 30 ns; -- testing mode 3
-    waddr   <= x"D110";
-    wdata   <= x"00060030";
-    wupdate <= '1';
-    wait for 40 ns;
-    waddr   <= x"D114";
-    wdata   <= x"00000003";
-    wupdate <= '1';
-    wait for 30 ns;
-    waddr   <= x"D118";
+    wait for 20 ns;
+    waddr   <= x"D004";
     wdata   <= x"00000001";
     wupdate <= '1';
-    wait for 100 ns;
-    waddr   <= x"D114";
+    wait for 10 ns;
+    waddr   <= x"0000";
     wdata   <= x"00000000";
-    wupdate <= '1';
+    wupdate <= '0';
     wait;
   end process;
 
-  rapid_read_process : process
+  read_process : process
   begin
     raddr   <= x"0000";
     rupdate <= '0';
-    wait for 42 ns;
-    raddr   <= x"D10C";
+    wait for 1 ns;
+    wait for 20 ns;
+    raddr   <= x"D000";
     rupdate <= '1';
     wait for 10 ns;
-    raddr   <= x"D100";
+    raddr   <= x"D004";
     rupdate <= '1';
     wait for 10 ns;
-    raddr   <= x"D104";
-    rupdate <= '1';
-    wait for 10 ns;
-    raddr   <= x"D110";
-    rupdate <= '1';
-    wait for 100 ns;
-    raddr   <= x"D100";
-    rupdate <= '1';
-    wait for 10 ns;
-    raddr   <= x"D10C";
-    rupdate <= '1';
-    wait for 30 ns;
-    raddr   <= x"D1FF";
-    rupdate <= '1';
-    wait for 10 ns;
-    raddr   <= x"D108";
+    raddr   <= x"D010";
     rupdate <= '1';
     wait for 10 ns;
     raddr   <= x"0000";
@@ -195,122 +143,45 @@ write_process : process
 
   data_in : process
   begin
-    di     <= x"000";
-    diof   <= '0';
-    wait for 28 ns;
-    di     <= x"111";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"222";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"333";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"444";
-    diof   <= '1';
-    wait for 10 ns;
-    di     <= x"555";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"666";
-    diof   <= '0';
-    --wait for 10 ns;
-    --di     <= x"777";
-    --diof   <= '0';
-    --wait for 10 ns;
-    --di     <= x"888";
-    --diof   <= '1';
-    wait for 10 ns;
-    di     <= x"999";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"AAA";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"BBB";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"CCC";
-    diof   <= '1';
-    wait for 10 ns;
-    di     <= x"DDD";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"EEE";
-    diof   <= '0';
-    wait for 10 ns;
     di     <= x"FFF";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"001";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"112";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"223";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"334";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"445";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"556";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"667";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"778";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"889";
     diof   <= '1';
-    wait for 10 ns;
-    di     <= x"99A";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"AAB";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"BBC";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"CCD";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"DDE";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"EEF";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"FF0";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"ACE";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"222";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"333";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"444";
-    diof   <= '1';
-    wait for 10 ns;
-    di     <= x"555";
-    diof   <= '0';
-    wait for 10 ns;
-    di     <= x"666";
-    diof   <= '0';
     wait;
   end process;
 
+  output_process : process
+    variable l : line;
+  begin
+    if (count < 15) then
+      wait for 10 ns;
+    else
+      wait;
+    end if;
+    write (l, String'("c: "));
+    write (l, count, left, 4);
+    write (l, String'(" || ra: 0x"));
+    hwrite (l, raddr);
+    write (l, String'(" ru:"));
+    write (l, rupdate);
+    write (l, String'(" rd: 0x"));
+    hwrite (l, rdata);
+    write (l, String'(" rk:"));
+    write (l, rack);
+    write (l, String'(" || wa: 0x"));
+    hwrite (l, waddr);
+    write (l, String'(" wu:"));
+    write (l, wupdate);
+    write (l, String'(" wd: 0x"));
+    hwrite (l, wdata);
+    write (l, String'(" wk:"));
+    write (l, wack);
+    write (l, String'(" || en:"));
+    write (l, adc_en);
+
+    if (rst = '1') then
+      write (l, String'(" (RESET)"));
+    end if;
+    writeline(output, l);
+  end process;
 
 
 end behaviour;
