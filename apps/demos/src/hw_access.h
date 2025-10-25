b77c4b531dd5410b7c1be8c5a853e0df0a7beea8
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-10-25 12:10:24 -0700

    cheanup hw_access.h in preparation for extension

--- apps/demos/src/hw_access.h
+++ apps/demos/src/hw_access.h
@@ -1,41 +1,75 @@
-#ifndef __HW_ACCESS_H__
-#define __HW_ACCESS_H__
-
-#include "xil_types.h"
-#include "xil_printf.h"
-#include "xil_cache.h"
-#include "sleep.h"
-//#include "xtime_l.h"
-//#include "xstatus.h"
+#ifndef HW_ACCESS_H
+#define HW_ACCESS_H
 
 //
 // Platform-dependent (Linux or Bare-Metal) access to the hardware.
-// *** This is the header for bare-metal applications ***
 //
 // This interface is implemented separately for Bare-Metal and Linux
-// application.  Code written to this interface should compile and run
-// unmodified on both Linux and bare-metal platforms.
+// applications.  Code written to this interface should compile and
+// run unmodified on both Linux and bare-metal platforms.
 //
 // The HW interfaces that are currently supported are: AXI-Lite PACMAN
 // register access, DMA register access, and memory mapped buffers for
-// DMA.
+// DMA.  (Soon: I2C)
 //
 
-// Hardware status is non-zero for any error:
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Includes:
+// For baremetal, we depart from standard library for types, printf, and sleep.
+#ifdef _LINUX
+  #include <stdint.h>
+  #include <stdio.h>
+  #include <unistd.h>
+#else
+  #include "xil_types.h"
+  #include "xil_printf.h"
+  #include "xil_cache.h"
+  #include "sleep.h"
+#endif
+
+// Typedefs
+#ifdef _LINUX
+  typedef uint32_t hw_addr_t;
+  typedef uint32_t hw_val_t;
+  typedef volatile uint32_t * hw_ptr_t;
+#else
+  typedef u32 hw_addr_t;
+  typedef u32 hw_val_t;
+  typedef volatile u32 * hw_ptr_t;
+#endif
+
+// Base addresses
+#ifdef _LINUX
+  #define AXIL_REGISTERS_BASEADDR 0x40000000
+  #define DMA_REGISTERS_BASEADDR  0x40400000
+#else
+  #define AXIL_REGISTERS_BASEADDR XPAR_AXIL_TO_REGBUS_0_BASEADDR
+  #define DMA_REGISTERS_BASEADDR  XPAR_AXI_DMA_0_BASEADDR
+#endif
+
+// Cache management macros
+#ifdef _LINUX
+  #define HW_FLUSH_DCACHE(ptr, len)      ((void)0)
+  #define HW_INVALIDATE_DCACHE(ptr, len) ((void)0)
+#else
+  #define HW_FLUSH_DCACHE(ptr, len)      Xil_DCacheFlushRange((UINTPTR)(ptr), (len))
+  #define HW_INVALIDATE_DCACHE(ptr, len) Xil_DCacheInvalidateRange((UINTPTR)(ptr), (len))
+#endif
+
+// printf mapping
+#ifndef _LINUX
+  #define printf xil_printf
+#endif
+
 
 #define HW_SUCCESS 0
 
-// Typedefs for hardware addresses, values, and pointers:
-typedef u32 hw_addr_t;
-typedef u32 hw_val_t;
-typedef volatile u32 * hw_ptr_t;
-
 //
-// AXI-Lite Registers:
+// AXI-LITE (general purpose) register access:
 //
-
-// Base Hardware Address for the AXI-Lite Interface to PACMAN Registers
-#define AXIL_REGISTERS_BASEADDR  XPAR_AXIL_TO_REGBUS_0_BASEADDR
 
 // initialize the AXI-LITE interface for register access:
 void init_axil_driver();
@@ -55,9 +89,6 @@ void     axil_write_register (hw_addr_t offset, hw_val_t value);
 //
 // DMA Registers:
 //
-
-// Base Hardware Address for the AXI-Lite Interface to DMA
-#define DMA_REGISTERS_BASEADDR XPAR_AXI_DMA_0_BASEADDR
 
 // initialize the software driver for the AXI-LITE interface providing register access:
 void init_dma_driver();
@@ -80,16 +111,7 @@ void     dma_write_register (hw_addr_t offset, hw_val_t value);
 
 void init_dma_buffer(hw_addr_t baseaddr, hw_addr_t size);
 
-#define HW_FLUSH_DCACHE(ptr, len)      Xil_DCacheFlushRange((UINTPTR)(ptr), (len))
-#define HW_INVALIDATE_DCACHE(ptr, len) Xil_DCacheInvalidateRange((UINTPTR)(ptr), (len))
-
 hw_ptr_t dma_ptr(hw_addr_t addr);
-
-
-//
-// printf utility: uses xil_printf
-//
-#define printf xil_printf
 
 //
 // timer utility:
@@ -97,6 +119,10 @@ hw_ptr_t dma_ptr(hw_addr_t addr);
 void start_hw_timer();
 void stop_hw_timer();
 unsigned hw_timer_elapsed_us();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __HW_ACCESS_H__
 
--- petalinux/pkg/pacman-apps/files/src/platform/hw_access.h
+++ petalinux/pkg/pacman-apps/files/src/platform/hw_access.h
@@ -1,42 +1,75 @@
-#ifndef __HW_ACCESS_H__
-#define __HW_ACCESS_H__
+#ifndef HW_ACCESS_H
+#define HW_ACCESS_H
 
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
+//
+// Platform-dependent (Linux or Bare-Metal) access to the hardware.
+//
+// This interface is implemented separately for Bare-Metal and Linux
+// applications.  Code written to this interface should compile and
+// run unmodified on both Linux and bare-metal platforms.
+//
+// The HW interfaces that are currently supported are: AXI-Lite PACMAN
+// register access, DMA register access, and memory mapped buffers for
+// DMA.  (Soon: I2C)
+//
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-//
-// Platform-dependent (Linux or Bare-Metal) access to the hardware.
-// *** This is the header for bare-metal applications ***
-//
-// This interface is implemented separately for Bare-Metal and Linux
-// application.  Code written to this interface should compile and run
-// unmodified on both Linux and bare-metal platforms.
-//
-// The HW interfaces that are currently supported are: AXI-Lite PACMAN
-// register access, DMA register access, and memory mapped buffers for
-// DMA.
-//
+// Includes:
+// For baremetal, we depart from standard library for types, printf, and sleep.
+#ifdef _LINUX
+  #include <stdint.h>
+  #include <stdio.h>
+  #include <unistd.h>
+#else
+  #include "xil_types.h"
+  #include "xil_printf.h"
+  #include "xil_cache.h"
+  #include "sleep.h"
+#endif
 
-// Hardware status is non-zero for any error:
+// Typedefs
+#ifdef _LINUX
+  typedef uint32_t hw_addr_t;
+  typedef uint32_t hw_val_t;
+  typedef volatile uint32_t * hw_ptr_t;
+#else
+  typedef u32 hw_addr_t;
+  typedef u32 hw_val_t;
+  typedef volatile u32 * hw_ptr_t;
+#endif
+
+// Base addresses
+#ifdef _LINUX
+  #define AXIL_REGISTERS_BASEADDR 0x40000000
+  #define DMA_REGISTERS_BASEADDR  0x40400000
+#else
+  #define AXIL_REGISTERS_BASEADDR XPAR_AXIL_TO_REGBUS_0_BASEADDR
+  #define DMA_REGISTERS_BASEADDR  XPAR_AXI_DMA_0_BASEADDR
+#endif
+
+// Cache management macros
+#ifdef _LINUX
+  #define HW_FLUSH_DCACHE(ptr, len)      ((void)0)
+  #define HW_INVALIDATE_DCACHE(ptr, len) ((void)0)
+#else
+  #define HW_FLUSH_DCACHE(ptr, len)      Xil_DCacheFlushRange((UINTPTR)(ptr), (len))
+  #define HW_INVALIDATE_DCACHE(ptr, len) Xil_DCacheInvalidateRange((UINTPTR)(ptr), (len))
+#endif
+
+// printf mapping
+#ifndef _LINUX
+  #define printf xil_printf
+#endif
+
 
 #define HW_SUCCESS 0
 
-// Typedefs for hardware addresses, values, and pointers:
-typedef uint32_t hw_addr_t;
-typedef uint32_t hw_val_t;
-typedef volatile uint32_t * hw_ptr_t;
-
 //
-// AXI-Lite Registers:
+// AXI-LITE (general purpose) register access:
 //
-
-// Base Hardware Address for the AXI-Lite Interface to PACMAN Registers
-#define AXIL_REGISTERS_BASEADDR  0x40000000
 
 // initialize the AXI-LITE interface for register access:
 void init_axil_driver();
@@ -56,9 +89,6 @@ void     axil_write_register (hw_addr_t offset, hw_val_t value);
 //
 // DMA Registers:
 //
-
-// Base Hardware Address for the AXI-Lite Interface to DMA
-#define DMA_REGISTERS_BASEADDR 0x40400000
 
 // initialize the software driver for the AXI-LITE interface providing register access:
 void init_dma_driver();
@@ -81,11 +111,7 @@ void     dma_write_register (hw_addr_t offset, hw_val_t value);
 
 void init_dma_buffer(hw_addr_t baseaddr, hw_addr_t size);
 
-#define HW_FLUSH_DCACHE(ptr, len)      ((void)0)
-#define HW_INVALIDATE_DCACHE(ptr, len) ((void)0)
-
 hw_ptr_t dma_ptr(hw_addr_t addr);
-
 
 //
 // timer utility:
