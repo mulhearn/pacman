e9618558adac3d78a47ec27a269b5855c356fa52
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2025-11-17 02:38:27 -0800

    larpix moved out of pacman repo

--- larpix_control/larpix_control/common/__init__.py
+++
@@ -1,5 +0,0 @@
-from .helpers import dict_from_yaml           # noqa: F401
-from .helpers import validate_required_keys   # noqa: F401
-from .helpers import validate_required_lists  # noqa: F401
-from .helpers import validate_required_dicts  # noqa: F401
-from .interfaces import asic_config_iface     # noqa: F401
--- larpix_control/test/test_import.py
+++
@@ -1,8 +0,0 @@
-# test_import.py
-
-try:
-    import larpix_control
-    from larpix_control.common.helpers import dict_from_yaml
-    print("Import successful!")
-except ImportError as e:
-    print("Import failed:", e)
