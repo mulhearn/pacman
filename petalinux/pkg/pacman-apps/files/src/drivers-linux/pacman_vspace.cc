c0289e8a57391487a3c0de2d30796b442ea80298
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2026-02-12 11:29:15 -0800

    all registers are treated as virtual during legacy DAQ integration

--- petalinux/pkg/pacman-apps/files/src/drivers-linux/pacman_vspace.cc
+++ petalinux/pkg/pacman-apps/files/src/drivers-linux/pacman_vspace.cc
@@ -9,111 +9,122 @@
 
 
 int pacman_vspace_write(uint32_t addr, uint32_t value){
+  unsigned tmp, off;
   printf("DEBUG: vspace_write: addr 0x%x value 0x%x \r\n", addr, value);
 
   if (addr >= PACMAN_VSPACE_I2C_START) {
-    unsigned off = addr - PACMAN_VSPACE_I2C_START;
+    off = addr - PACMAN_VSPACE_I2C_START;
     printf("DEBUG: vspace_write: virtual I2C write at offset 0x%x value 0x%x \r\n", off, value);
     return i2c_write(off, value);
   }
 
+  // for debugging legacy DAQ with new driver, we are treating all non-I2C registers as vitual:
   if (addr >= PACMAN_VSPACE_REG_START){
-    unsigned tmp = 0;
-    unsigned off = addr - PACMAN_VSPACE_REG_START;
+    off = addr - PACMAN_VSPACE_REG_START;
     printf("DEBUG: vspace_write: virtual reg write at offset 0x%x value 0x%x \r\n", off, value);
+  } else {
+    off = addr;
+    printf("DEBUG: vspace_write: treating HW reg as virtual reg write at offset 0x%x value 0x%x \r\n", off, value);
+  }
 
-    switch(off){
-    case 0x0010:
-      tmp = pacman_read(0xF010);
-      tmp &= 0xFFFF0000;
-      tmp |= (value & 0x03FF);
-      return pacman_write(0xF010, tmp);
-    case 0x0014:
-      tmp = pacman_read(0xF010);
-      tmp &= 0xFFF0FFFF;
-      if (value&0x1)
-	tmp |= 0x00010000;
-      return pacman_write(0xF010, tmp);
-    case 0x0018:
-      // ignoring... already configured correctly.
-      return EXIT_SUCCESS;
-    case 0x001C:
-      // ignoring... already configured correctly.
-      return EXIT_SUCCESS;
-    case 0x1010:
-      // this is a request to send a sync pulse:
-      if ((value&0x4)!=0){
-	// use Poke C register (mapped to SYNC pulse in config)
-	return pacman_write(0xE0C0, 0x0);
-      }
-      return EXIT_SUCCESS;
-    case 0x1014:
-      // ignoring... already configured correctly.
-      return EXIT_SUCCESS;
-    case 0x1018:
-      // ignoring... already configured correctly.
-      return EXIT_SUCCESS;
-    case 0x101C:
-      // ignoring... already configured correctly.
-      return EXIT_SUCCESS;
-    case 0x2014:
-      // ignoring...
-      return EXIT_SUCCESS;
+  switch(off){
+  case 0x0010:
+    tmp = pacman_read(0xF010);
+    tmp &= 0xFFFF0000;
+    tmp |= (value & 0x03FF);
+    return pacman_write(0xF010, tmp);
+  case 0x0014:
+    tmp = pacman_read(0xF010);
+    tmp &= 0xFFF0FFFF;
+    if (value&0x1)
+      tmp |= 0x00010000;
+    return pacman_write(0xF010, tmp);
+  case 0x0018:
+    // ignoring... already configured correctly.
+    return EXIT_SUCCESS;
+  case 0x001C:
+    // ignoring... already configured correctly.
+    return EXIT_SUCCESS;
+  case 0x1010:
+    // this is a request to send a sync pulse:
+    if ((value&0x4)!=0){
+      // use Poke C register (mapped to SYNC pulse in config)
+      return pacman_write(0xE0C0, 0x0);
     }
-
-    // non-virtual addesses: silently ignore:
+    return EXIT_SUCCESS;
+  case 0x1014:
+    // ignoring... already configured correctly.
+    return EXIT_SUCCESS;
+  case 0x1018:
+    // ignoring... already configured correctly.
+    return EXIT_SUCCESS;
+  case 0x101C:
+    // ignoring... already configured correctly.
+    return EXIT_SUCCESS;
+  case 0x2014:
+    // ignoring...
     return EXIT_SUCCESS;
   }
 
+  // silently ignore anything not explicitly handled:
+  return EXIT_SUCCESS;
+
   // non-virtual address:
-  printf("DEBUG: vspace_write: non-virtual reg write at offset 0x%x value 0x%x \r\n", addr, value);
-  return pacman_write(addr, value);
+  //printf("DEBUG: vspace_write: non-virtual reg write at offset 0x%x value 0x%x \r\n", addr, value);
+  //return pacman_write(addr, value);
 
 }
 
 uint32_t pacman_vspace_read(uint32_t addr, int * status){
+  unsigned off, tmp;
+
   if (status)
     *status = EXIT_SUCCESS;
 
   printf("DEBUG: vspace_read addr 0x%x\r\n",addr);
 
   if (addr >= PACMAN_VSPACE_I2C_START) {
-    unsigned off = addr - PACMAN_VSPACE_I2C_START;
+    off = addr - PACMAN_VSPACE_I2C_START;
     printf("DEBUG: vspace_read:  I2C read at offset 0x%x\r\n", off);
     return i2c_read(off);
   }
 
+  // for debugging legacy DAQ with new driver, we are treating all non-I2C registers as vitual:
   if (addr >= PACMAN_VSPACE_REG_START){
-    unsigned tmp = 0;
-    unsigned off = addr - PACMAN_VSPACE_REG_START;
+    off = addr - PACMAN_VSPACE_REG_START;
     printf("DEBUG: vspace_read: virtual reg read at offset 0x%x \r\n", off);
-
-    switch(addr){
-    case 0x0000:
-      return pacman_read(0xFF10, status);
-    case 0x0004:
-      return pacman_read(0xFF14, status);
-    case 0x0008:
-      return pacman_read(0xFF18, status);
-    case 0x000C:
-      return pacman_read(0xFF1C, status);
-    case 0x0010:
-      tmp = pacman_read(0xF010, status);
-      tmp &= 0x000003FF;
-      return tmp;
-    case 0x0014:
-      tmp = pacman_read(0xF010);
-      return ((tmp & 0x00010000) != 0);
-    case 0x1000:
-      return 0;
-    case 0x1010:
-      return 0;
-    }
-    return 0;
+  } else {
+    off = addr;
+    printf("DEBUG: vspace_read: treating HW reg as virtual reg read at offset 0x%x \r\n", off);
   }
 
+  tmp = 0;
+  switch(addr){
+  case 0x0000:
+    return pacman_read(0xFF10, status);
+  case 0x0004:
+    return pacman_read(0xFF14, status);
+  case 0x0008:
+    return pacman_read(0xFF18, status);
+  case 0x000C:
+    return pacman_read(0xFF1C, status);
+  case 0x0010:
+    tmp = pacman_read(0xF010, status);
+    tmp &= 0x000003FF;
+    return tmp;
+  case 0x0014:
+    tmp = pacman_read(0xF010);
+    return ((tmp & 0x00010000) != 0);
+  case 0x1000:
+    return 0;
+  case 0x1010:
+    return 0;
+  }
+  // return 0 for registers not explicitly handled.
+  return 0;
+
   // non-virtual address:
-  printf("DEBUG: vspace_read: non-virtual reg read at address 0x%x \r\n", addr);
-  return pacman_read(addr, status);
+  //printf("DEBUG: vspace_read: non-virtual reg read at address 0x%x \r\n", addr);
+  //return pacman_read(addr, status);
 
 }
