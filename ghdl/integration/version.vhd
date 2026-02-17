a0c84f1681ce634615a277c27c82cb2811639cda
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2026-02-16 23:53:39 -0800

    bump fimware version to 3.5.0

--- ghdl/integration/version.vhd
+++ ghdl/integration/version.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 package version is
   constant C_FIRMWARE_MAJOR  : integer  := 3;
-  constant C_FIRMWARE_MINOR  : integer  := 4;
+  constant C_FIRMWARE_MINOR  : integer  := 5;
   constant C_FIRMWARE_LETTER : integer  := 0;
   constant C_HARDWARE_MAJOR  : integer  := 1;
   constant C_HARDWARE_MINOR  : integer  := 5;
