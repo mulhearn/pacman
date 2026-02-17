566d16be81854721d8b455b0769b964f6a427ed2
Michael Mulhearn <5119251+mulhearn@users.noreply.github.com>
2026-02-17 00:21:45 -0800

    sync with linux

--- apps/demos/src/iic_menu.c
+++ apps/demos/src/iic_menu.c
@@ -1,10 +1,27 @@
 #include <stdlib.h>
 
 #include "hw_access.h"
-//#include "global.h"
 #include "iic.h"
 #include "iic_menu.h"
 
+void iic_toggle_hardware_version(){
+  static int mode = 0;
+  mode = (mode + 1) % 4;
+
+  if (mode==0){
+    printf("INFO: setting I2C HW version to latest (PACMAN 1V5)\r\n");
+    iic_set_hw_version(1,5,0);
+  } else if (mode==1){
+    printf("INFO: setting I2C HW version to PACMAN 1V4 \r\n");
+    iic_set_hw_version(1,4,0);
+  } else if (mode==2){
+    printf("INFO: setting I2C HW version to PACMAN 1V3 \r\n");
+    iic_set_hw_version(1,3,0);
+  } else {
+    printf("INFO: setting I2C HW version to disabled \r\n");
+    iic_set_hw_version(0,0,0);
+  }
+}
 
 void iic_toggle_power(){
   unsigned vddd[] = {0x00, 0x2000, 0x4000, 0x8000, 0xFFFF};
@@ -14,12 +31,12 @@ void iic_toggle_power(){
 
   printf("INFO: setting VDDD to 0x%x \r\n", vddd[mode]);
   for (int i=0; i<10; i++){
-    iic_set_vddd(i, vddd[mode]);
+    iic_set_vddd_dn(i, vddd[mode]);
   }
 
   printf("INFO: setting VDDA to 0x%x \r\n", vdda[mode]);
   for (int i=0; i<10; i++){
-    iic_set_vdda(i, vdda[mode]);
+    iic_set_vdda_dn(i, vdda[mode]);
   }
 }
 
@@ -56,15 +73,15 @@ void iic_menu(){
   printf("I2C menu:  \r\n");
   while(1){
     printf("choose an option:\r\n");
-    printf("(x) Exit I2C menu (c) check I2C (p) toggle power (m) monitor power \r\n");
+    printf("(x) Exit I2C menu (v) toggle I2C HW version (c) check I2C (p) toggle power (m) monitor power \r\n");
     char input = input_choice();
     printf("INFO: selected %c\r\n", input);
 
     switch(input){
     case 'x':
       return;
-    case 'c':
-      check_iic();
+    case 'v':
+      iic_toggle_hardware_version();
       break;
     case 'p':
       iic_toggle_power();
